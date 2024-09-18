daf1a801e4210341ca8e4195d86d3061eb3f7201
darrin@darrinduncan.com <darrin@darrinduncan.com>
2024-09-17 20:43:26 -0400

    Navigation fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,19 +2,21 @@ import Menu from './Menu'
 import logo from '../assets/logo.png'
 
 const Navigation = () => {
-  return (
-    <nav className="navbar fixed-top navbar-expand-lg navbar-light main-nav">
-  <div className="container">
-    <a className="navbar-brand" href="#"><img src={logo} alt="Darrin Duncan | Front-end Developer" /></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <Menu />
-    </div>
-  </div>
-</nav>
-  )
+    return (
+        <div className="container">
+            <nav className="navbar fixed-top navbar-expand-lg navbar-light main-nav">
+                <div className="container">
+                    <a className="navbar-brand" href="#"><img src={logo} alt="Darrin Duncan | Front-end Developer" /></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <Menu />
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Navigation
